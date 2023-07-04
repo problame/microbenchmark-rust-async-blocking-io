d28f8fbed6efbb0980cdb1b208d7314f82256dfa
Christian Schwarz <me@cschwarz.com>
2023-07-04 09:59:15 +0000

    engine for on-executor-thread

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,7 @@ enum DiskAccessKind {
 #[derive(Clone, Copy, clap::Subcommand)]
 enum EngineKind {
     Std,
+    TokioOnExecutorThread,
     TokioSpawnBlocking {
         spawn_blocking_pool_size: NonZeroU64,
     },
@@ -257,6 +258,13 @@ fn setup_files(args: &Args, disk_access_kind: &DiskAccessKind) {
 fn setup_engine(engine_kind: &EngineKind) -> Arc<dyn Engine> {
     match engine_kind {
         EngineKind::Std => Arc::new(EngineStd {}),
+        EngineKind::TokioOnExecutorThread => {
+            let rt = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .unwrap();
+            Arc::new(EngineTokioOnExecutorThread { rt })
+        }
         EngineKind::TokioSpawnBlocking {
             spawn_blocking_pool_size,
         } => {
@@ -413,6 +421,97 @@ impl EngineStd {
         debug_assert_eq!(buf.len(), args.block_size_shift.get() as usize);
         file.read_at(buf, offset).unwrap();
         // TODO: verify
+    }
+}
+
+struct EngineTokioOnExecutorThread {
+    rt: tokio::runtime::Runtime,
+}
+
+impl Engine for EngineTokioOnExecutorThread {
+    fn run(
+        self: Arc<Self>,
+        args: &'static Args,
+        works: Vec<ClientWork>,
+        stop: Arc<AtomicBool>,
+        stats_state: Arc<StatsState>,
+    ) {
+        let rt = &self.rt;
+        rt.block_on(async {
+            let mut handles = Vec::new();
+            assert_eq!(works.len(), args.num_clients.get() as usize);
+            let all_client_tasks_spawned =
+                Arc::new(tokio::sync::Barrier::new(args.num_clients.get() as usize));
+            for (i, work) in (0..args.num_clients.get()).zip(works.into_iter()) {
+                let stop = Arc::clone(&stop);
+                let stats_state = Arc::clone(&stats_state);
+                let all_client_tasks_spawned = Arc::clone(&all_client_tasks_spawned);
+                handles.push(tokio::spawn(async move {
+                    Self::client(i, &args, work, &stop, stats_state, all_client_tasks_spawned).await
+                }));
+            }
+            for handle in handles {
+                handle.await.unwrap();
+            }
+        });
+    }
+}
+
+impl EngineTokioOnExecutorThread {
+    async fn client(
+        i: u64,
+        args: &Args,
+        mut work: ClientWork,
+        stop: &AtomicBool,
+        stats_state: Arc<StatsState>,
+        all_client_tasks_spawned: Arc<tokio::sync::Barrier>,
+    ) {
+        tracing::info!("Client {i} starting");
+        all_client_tasks_spawned.wait().await;
+        let block_size = 1 << args.block_size_shift.get();
+
+        let rwlock = Arc::new(tokio::sync::RwLock::new(()));
+        std::mem::forget(Arc::clone(&rwlock));
+
+        // alloc aligned to make O_DIRECT work
+        let buf = {
+            let buf_ptr = unsafe {
+                std::alloc::alloc(Layout::from_size_align(block_size, block_size).unwrap())
+            };
+            assert!(!buf_ptr.is_null());
+            let buf: &mut [u8] = unsafe { std::slice::from_raw_parts_mut(buf_ptr, block_size) };
+            buf
+        };
+        let block_size_u64: u64 = block_size.try_into().unwrap();
+        while !stop.load(Ordering::Relaxed) {
+            // simulate Timeline::layers.read().await
+            let _guard = rwlock.read().await;
+
+            // find a random aligned 8k offset inside the file
+            debug_assert!(1024 * 1024 % block_size == 0);
+            let offset_in_file = rand::thread_rng()
+                .gen_range(0..=((args.file_size_mib.get() * 1024 * 1024 - 1) / block_size_u64))
+                * block_size_u64;
+            let start = std::time::Instant::now();
+            match &mut work {
+                ClientWork::DiskAccess { file } => {
+                    file.read_at(buf, offset_in_file).unwrap();
+                }
+                ClientWork::TimerFd { timerfd } => {
+                    timerfd.read();
+                }
+            }
+            stats_state
+                .reads_in_last_second
+                .fetch_add(1, Ordering::Relaxed);
+            // if i == 0 {
+            // info!("Client {i} read took {:?}", start.elapsed());
+            stats_state
+                .client0_latency_sample
+                .store(start.elapsed().as_micros() as u64, Ordering::Relaxed);
+            // }
+        }
+        info!("Client {i} stopping");
     }
 }
 
