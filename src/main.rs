a820bf39c2472750bc359c4aa733798faadc60d0
Christian Schwarz <me@cschwarz.com>
2023-07-06 16:52:01 +0000

    add tokio+rio-based engine

--- src/main.rs
+++ src/main.rs
@@ -90,9 +90,14 @@ enum EngineKind {
         workers: NonZeroU64,
         queue_depth: NonZeroU64,
     },
+    TokioRio,
 }
 
 struct EngineStd {}
+
+struct EngineTokioRio {
+    rt: tokio::runtime::Runtime,
+}
 
 struct EngineTokioSpawnBlocking {
     rt: tokio::runtime::Runtime,
@@ -310,6 +315,12 @@ fn setup_engine(engine_kind: &EngineKind) -> Arc<dyn Engine> {
                 queue_depth: *queue_depth,
             })
         }
+        EngineKind::TokioRio => Arc::new(EngineTokioRio {
+            rt: tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .unwrap(),
+        }),
     }
 }
 
@@ -844,5 +855,122 @@ impl EngineTokioFlume {
                 }
             }
         }
+    }
+}
+
+impl Engine for EngineTokioRio {
+    fn run(
+        self: Arc<Self>,
+        args: &'static Args,
+        works: Vec<ClientWork>,
+        stop: Arc<AtomicBool>,
+        stats_state: Arc<StatsState>,
+    ) {
+        let rio = rio::new().unwrap();
+
+        self.rt.block_on(async {
+            let mut handles = Vec::new();
+            for (i, work) in (0..args.num_clients.get()).zip(works.into_iter()) {
+                let stop = Arc::clone(&stop);
+                let stats_state = Arc::clone(&stats_state);
+                let myself = Arc::clone(&self);
+                let rio = rio.clone();
+                handles.push(tokio::spawn(async move {
+                    myself.client(rio, i, &args, work, &stop, stats_state).await
+                }));
+            }
+            for handle in handles {
+                handle.await.unwrap();
+            }
+        });
+    }
+}
+
+impl EngineTokioRio {
+    async fn client(
+        &self,
+        rio: rio::Rio,
+        i: u64,
+        args: &Args,
+        work: ClientWork,
+        stop: &AtomicBool,
+        stats_state: Arc<StatsState>,
+    ) {
+        tracing::info!("Client {i} starting");
+        let block_size = 1 << args.block_size_shift.get();
+
+        let rwlock = Arc::new(tokio::sync::RwLock::new(()));
+        std::mem::forget(Arc::clone(&rwlock));
+
+        #[derive(Copy, Clone)]
+        enum ClientWorkFd {
+            DiskAccess(RawFd),
+            TimerFd(RawFd, Duration),
+            NoWork,
+        }
+
+        let fd = match work {
+            ClientWork::DiskAccess { file } => ClientWorkFd::DiskAccess(file.into_raw_fd()),
+            ClientWork::TimerFdSetStateAndRead { timerfd, duration } => {
+                let ret = ClientWorkFd::TimerFd(timerfd.as_raw_fd(), duration);
+                std::mem::forget(timerfd); // they don't support into_raw_fd
+                ret
+            }
+            ClientWork::NoWork {} => ClientWorkFd::NoWork,
+        };
+
+        // alloc aligned to make O_DIRECT work
+        let buf = {
+            let buf_ptr = unsafe {
+                std::alloc::alloc(Layout::from_size_align(block_size, block_size).unwrap())
+            };
+            assert!(!buf_ptr.is_null());
+            #[derive(Clone, Copy)]
+            struct SendPtr(*mut u8);
+            unsafe impl Send for SendPtr {} // the thread spawned in the loop below doesn't outlive this function (we're polled t completion)
+            unsafe impl Sync for SendPtr {} // the loop below ensures only one thread accesses it at a time
+            let buf = SendPtr(buf_ptr);
+            // extra scope so that it doesn't outlive any await points, it's not Send, only the SendPtr wrapper is
+            buf
+        };
+        let block_size_u64: u64 = block_size.try_into().unwrap();
+        while !stop.load(Ordering::Relaxed) {
+            // simulate Timeline::layers.read().await
+            let _guard = rwlock.read().await;
+
+            // find a random aligned 8k offset inside the file
+            debug_assert!(1024 * 1024 % block_size == 0);
+            let offset_in_file = rand::thread_rng()
+                .gen_range(0..=((args.file_size_mib.get() * 1024 * 1024 - 1) / block_size_u64))
+                * block_size_u64;
+
+            let start = std::time::Instant::now();
+            match fd {
+                ClientWorkFd::DiskAccess(file_fd) => {
+                    let buf = buf;
+                    let mut buf: &mut [u8] =
+                        unsafe { std::slice::from_raw_parts_mut(buf.0, block_size) };
+                    let file = unsafe { std::fs::File::from_raw_fd(file_fd) };
+                    let count = rio.read_at(&file, &mut buf, offset_in_file).await.unwrap();
+                    assert_eq!(count, buf.len());
+                    file.into_raw_fd(); // so that it's there for next iteration
+                }
+                ClientWorkFd::TimerFd(_timerfd, _duration) => {
+                    unimplemented!()
+                }
+                ClientWorkFd::NoWork => (),
+            }
+            // TODO: can this dealock with rendezvous channel, i.e., queue_depth=0?
+
+            stats_state
+                .reads_in_last_second
+                .fetch_add(1, Ordering::Relaxed);
+            if i == 0 {
+                stats_state
+                    .client0_latency_sample
+                    .store(start.elapsed().as_micros() as u64, Ordering::Relaxed);
+            }
+        }
+        info!("Client {i} stopping");
     }
 }
